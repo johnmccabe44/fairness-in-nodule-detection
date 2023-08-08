6998c7fac7833883e41e11bc0f2e9dd0f12a5dea
john.mccabe44 <j.mccabe@ucl.com>
2023-08-08 07:58:36 +0100

    Adapted sitk logging

--- models/detection/luna16_filecheck.py
+++ models/detection/luna16_filecheck.py
@@ -10,6 +10,71 @@ import SimpleITK as sitk
 import sys
 from typing import List
 
+class SimpleITKLogger(sitk.LoggerBase):
+    """
+    Adapts SimpleITK messages to be handled by a Python Logger object.
+
+    Allows using the logging module to control the handling of messages coming
+    from ITK and SimpleTK. Messages such as debug and warnings are handled by
+    objects derived from sitk.LoggerBase.
+
+    The LoggerBase.SetAsGlobalITKLogger method must be called to enable
+    SimpleITK messages to use the logger.
+
+    The Python logger module adds a second layer of control for the logging
+    level in addition to the controls already in SimpleITK.
+
+    The "Debug" property of a SimpleITK object must be enabled (if
+    available) and the support from the Python "logging flow" hierarchy
+    to handle debug messages from a SimpleITK object.
+
+    Warning messages from SimpleITK are globally disabled with
+    ProcessObject:GlobalWarningDisplayOff.
+    """
+
+    def __init__(
+        self, logger: logging.Logger = logging.getLogger("SimpleITK")
+    ):
+        """
+        Initializes with a Logger object to handle the messages emitted from
+        SimpleITK/ITK.
+        """
+        super(SimpleITKLogger, self).__init__()
+        self._logger = logger
+
+    @property
+    def logger(self):
+        return self._logger
+
+    @logger.setter
+    def logger(self, logger):
+        self._logger = logger
+
+    def __enter__(self):
+        self._old_logger = self.SetAsGlobalITKLogger()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._old_logger.SetAsGlobalITKLogger()
+        del self._old_logger
+
+    def DisplayText(self, s):
+        # Remove newline endings from SimpleITK/ITK messages since the Python
+        # logger adds during output.
+        self._logger.info(s.rstrip())
+
+    def DisplayErrorText(self, s):
+        self._logger.error(s.rstrip())
+
+    def DisplayWarningText(self, s):
+        self._logger.warning(s.rstrip())
+
+    def DisplayGenericOutputText(self, s):
+        self._logger.info(s.rstrip())
+
+    def DisplayDebugText(self, s):
+        self._logger.debug(s.rstrip())
+
 def check_integrity(idx: int, mhd_paths: List[Path]):
 
     mhd_path = mhd_paths[idx]
@@ -48,6 +113,15 @@ if __name__ == '__main__':
         Programme entry point
     """
 
+    # Enable all debug messages for all ProcessObjects, and procedures
+    sitk.ProcessObject.GlobalDefaultDebugOn()
+
+    # Construct a SimpleITK logger to Python Logger adaptor
+    sitkLogger = SimpleITKLogger()
+
+    # Configure ITK to use the logger adaptor
+    sitkLogger.SetAsGlobalITKLogger()
+    
     logging.basicConfig(filename='sota-detection-checks.log', level=logging.INFO)
     mhd_root        = Path(sys.argv[1])
     sub_processes   = int(sys.argv[2])
