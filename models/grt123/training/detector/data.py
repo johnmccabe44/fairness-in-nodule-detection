30fc11367d0a88e25bdbc19e51f8bbdf6dde49f8
john.mccabe44 <j.mccabe@ucl.com>
2023-08-25 12:49:51 +0100

    Debugging for each crop

--- models/grt123/training/detector/data.py
+++ models/grt123/training/detector/data.py
@@ -108,6 +108,7 @@ class DataBowl3Detector(Dataset):
                 bboxes = self.sample_bboxes[int(bbox[0])]
                 isScale = self.augtype['scale'] and (self.phase=='train')
                 sample, target, bboxes, coord = self.crop(imgs, bbox[1:], bboxes, isScale, isRandom)
+                print(f'{filename},{sample.shape},{target.shape}, {bbox[1:]}, {bboxes}', flush=True)
                 if self.phase=='train' and not isRandom:
                      sample, target, bboxes, coord = augment(sample, target, bboxes, coord,
                         ifflip = self.augtype['flip'], ifrotate=self.augtype['rotate'], ifswap = self.augtype['swap'])
@@ -119,6 +120,8 @@ class DataBowl3Detector(Dataset):
                 bboxes = self.sample_bboxes[randimid]
                 isScale = self.augtype['scale'] and (self.phase=='train')
                 sample, target, bboxes, coord = self.crop(imgs, [], bboxes,isScale=False,isRand=True)
+
+                
             label = self.label_mapping(sample.shape[1:], target, bboxes, filename)
             sample = (sample.astype(np.float32)-128)/128
             #if filename in self.kagglenames and self.phase=='train':
