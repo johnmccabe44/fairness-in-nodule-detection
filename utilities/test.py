c19cbe7937e9f9df4ff7edc7523082330e2a320d
john.mccabe44 <j.mccabe@ucl.com>
2024-02-14 07:39:46 +0000

    Adding util tests

--- utilities/test.py
+++ utilities/test.py
@@ -1,46 +1,245 @@
 import numpy as np
 import os
 import sys
+from torch.utils.data import Dataset
 
-def main(datadir):
+class DataBowl3Detector(Dataset):
+    def __init__(self, data_dir, config, phase = 'train'):
+        self.phase = phase
+        self.max_stride = config['max_stride']       
+        self.stride = config['stride']       
+        sizelim = config['sizelim']/config['reso']
+        sizelim2 = config['sizelim2']/config['reso']
+        sizelim3 = config['sizelim3']/config['reso']
+        self.isScale = config['aug_scale']
+        self.r_rand = config['r_rand_crop']
+        self.augtype = config['augtype']
+        self.pad_value = config['pad_value']
 
-    print(np.__version__)
 
+        idcs = [
+            label_file
+            for label_file in os.listdir(data_dir)
+            if 'label' in label_file
+        ]
 
-    labels = []
+        labels = []
+        
+        for idx in idcs:
+            l = np.load(os.path.join(data_dir, '%s' %idx))
+            if np.all(l==0):
+                l=np.array([])
+            labels.append(l)
 
-    idcs = [label_file for label_file in os.listdir(data_dir) if 'label' in label_file]
+        self.sample_bboxes = labels
+        if self.phase != 'test':
+            self.bboxes = []
+            for i, l in enumerate(labels):
 
-    print(idcs[:10])
+                if len(l) > 0 :
+                    for t in l:
+                        if t[3]>sizelim:
+                            self.bboxes.append([np.concatenate([[i],t])])
+                        if t[3]>sizelim2:
+                            self.bboxes+=[[np.concatenate([[i],t])]]*2
+                        if t[3]>sizelim3:
+                            self.bboxes+=[[np.concatenate([[i],t])]]*4
 
-    sizelim = 1.
-    sizelim2 = 30
-    sizelim3 = 40
+            self.bboxes = np.concatenate(self.bboxes, axis=0)
 
-    for idx in idcs:
-        l = np.load(os.path.join(data_dir, '%s' %idx))
-        if np.all(l==0):
-            l=np.array([])
-        labels.append(l)
+        print(self.bboxes[:10])
+        self.crop = Crop(config)
+        self.label_mapping = LabelMapping(config, self.phase)
 
-    sample_bboxes = labels
-    bboxes = []
-    for i, l in enumerate(labels):
+    def __getitem__(self, idx, split=None):
+        return self.bboxes[idx]
 
-        if len(l) > 0 :
-            for t in l:
-                if t[3]>sizelim:
-                    bboxes.append([np.concatenate([[i],t])])
-                if t[3]>sizelim2:
-                    bboxes+=[[np.concatenate([[i],t])]]*2
-                if t[3]>sizelim3:
-                    bboxes+=[[np.concatenate([[i],t])]]*4
+    def __len__(self):
+        return len(self.bboxes)
+     
+class Crop(object):
+    def __init__(self, config):
+        self.crop_size = config['crop_size']
+        self.bound_size = config['bound_size']
+        self.stride = config['stride']
+        self.pad_value = config['pad_value']
+    def __call__(self, imgs, target, bboxes,isScale=False,isRand=False):
+        if isScale:
+            radiusLim = [8.,120.]
+            scaleLim = [0.75,1.25]
+            scaleRange = [np.min([np.max([(radiusLim[0]/target[3]),scaleLim[0]]),1])
+                         ,np.max([np.min([(radiusLim[1]/target[3]),scaleLim[1]]),1])]
+            scale = np.random.rand()*(scaleRange[1]-scaleRange[0])+scaleRange[0]
+            crop_size = (np.array(self.crop_size).astype('float')/scale).astype('int')
+        else:
+            crop_size=self.crop_size
+        bound_size = self.bound_size
+        target = np.copy(target)
+        bboxes = np.copy(bboxes)
+        
+        start = []
+        for i in range(3):
+            if not isRand:
+                r = target[3] / 2
+                s = np.floor(target[i] - r)+ 1 - bound_size
+                e = np.ceil (target[i] + r)+ 1 + bound_size - crop_size[i] 
+            else:
+                s = np.max([imgs.shape[i+1]-crop_size[i]/2,imgs.shape[i+1]/2+bound_size])
+                e = np.min([crop_size[i]/2,              imgs.shape[i+1]/2-bound_size])
+                target = np.array([np.nan,np.nan,np.nan,np.nan])
+            if s>e:
+                start.append(np.random.randint(e,s))#!
+            else:
+                start.append(int(target[i])-crop_size[i]/2+np.random.randint(-bound_size/2,bound_size/2))
+                
+                
+        normstart = np.array(start).astype('float32')/np.array(imgs.shape[1:])-0.5
+        normsize = np.array(crop_size).astype('float32')/np.array(imgs.shape[1:])
+        
+        xx,yy,zz = np.meshgrid(
+            np.linspace(normstart[0],normstart[0]+normsize[0],int(self.crop_size[0]/self.stride)),
+            np.linspace(normstart[1],normstart[1]+normsize[1],int(self.crop_size[1]/self.stride)),
+            np.linspace(normstart[2],normstart[2]+normsize[2],int(self.crop_size[2]/self.stride)),indexing ='ij'
+        )
+        
+        coord = np.concatenate([xx[np.newaxis,...], yy[np.newaxis,...],zz[np.newaxis,:]],0).astype('float32')
 
-    bboxes = np.concatenate(bboxes,axis = 0)
+        pad = []
+        pad.append([0,0])
+        for i in range(3):
+            leftpad = max(0,-start[i])
+            rightpad = max(0,start[i]+crop_size[i]-imgs.shape[i+1])
+            pad.append([leftpad,rightpad])
+        crop = imgs[:,
+            max(start[0],0):min(start[0] + crop_size[0],imgs.shape[1]),
+            max(start[1],0):min(start[1] + crop_size[1],imgs.shape[2]),
+            max(start[2],0):min(start[2] + crop_size[2],imgs.shape[3])]
+        crop = np.pad(crop,pad,'constant',constant_values =self.pad_value)
+        for i in range(3):
+            target[i] = target[i] - start[i] 
+        for i in range(len(bboxes)):
+            for j in range(3):
+                bboxes[i][j] = bboxes[i][j] - start[j] 
+                
+        if isScale:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                crop = zoom(crop,[1,scale,scale,scale],order=1)
+            newpad = self.crop_size[0]-crop.shape[1:][0]
+            if newpad<0:
+                crop = crop[:,:-newpad,:-newpad,:-newpad]
+            elif newpad>0:
+                pad2 = [[0,0],[0,newpad],[0,newpad],[0,newpad]]
+                crop = np.pad(crop,pad2,'constant',constant_values =self.pad_value)
+            for i in range(4):
+                target[i] = target[i]*scale
+            for i in range(len(bboxes)):
+                for j in range(4):
+                    bboxes[i][j] = bboxes[i][j]*scale
+        return crop, target, bboxes, coord
+    
+class LabelMapping(object):
+    def __init__(self, config, phase):
+        self.stride = np.array(config['stride'])
+        self.num_neg = int(config['num_neg'])
+        self.th_neg = config['th_neg']
+        self.anchors = np.asarray(config['anchors'])
+        self.phase = phase
+        if phase == 'train':
+            self.th_pos = config['th_pos_train']
+        elif phase == 'val':
+            self.th_pos = config['th_pos_val']
 
-    print(bboxes[:10])
+            
+    def __call__(self, input_size, target, bboxes, filename):
+        stride = self.stride
+        num_neg = self.num_neg
+        th_neg = self.th_neg
+        anchors = self.anchors
+        th_pos = self.th_pos
+        
+        output_size = []
+        for i in range(3):
+            assert input_size[i] % stride == 0, f'Error: index: {i}, input_size: {input_size[i]} is not divisiable by stride:{stride}, file:{filename}'
+            output_size.append(int(input_size[i] / stride))
+        
+        label = -1 * np.ones(output_size + [len(anchors), 5], np.float32)
+        offset = ((stride.astype('float')) - 1) / 2
+        oz = np.arange(offset, offset + stride * (output_size[0] - 1) + 1, stride)
+        oh = np.arange(offset, offset + stride * (output_size[1] - 1) + 1, stride)
+        ow = np.arange(offset, offset + stride * (output_size[2] - 1) + 1, stride)
+
+        for bbox in bboxes:
+            for i, anchor in enumerate(anchors):
+                iz, ih, iw = select_samples(bbox, anchor, th_neg, oz, oh, ow)
+                label[iz, ih, iw, i, 0] = 0
+
+        if self.phase == 'train' and self.num_neg > 0:
+            neg_z, neg_h, neg_w, neg_a = np.where(label[:, :, :, :, 0] == -1)
+            neg_idcs = random.sample(range(len(neg_z)), min(num_neg, len(neg_z)))
+            neg_z, neg_h, neg_w, neg_a = neg_z[neg_idcs], neg_h[neg_idcs], neg_w[neg_idcs], neg_a[neg_idcs]
+            label[:, :, :, :, 0] = 0
+            label[neg_z, neg_h, neg_w, neg_a, 0] = -1
+
+        if np.isnan(target[0]):
+            return label
+        iz, ih, iw, ia = [], [], [], []
+        for i, anchor in enumerate(anchors):
+            iiz, iih, iiw = select_samples(target, anchor, th_pos, oz, oh, ow)
+            iz.append(iiz)
+            ih.append(iih)
+            iw.append(iiw)
+            ia.append(i * np.ones((len(iiz),), np.int64))
+        iz = np.concatenate(iz, 0)
+        ih = np.concatenate(ih, 0)
+        iw = np.concatenate(iw, 0)
+        ia = np.concatenate(ia, 0)
+        flag = True 
+        if len(iz) == 0:
+            pos = []
+            for i in range(3):
+                pos.append(max(0, int(np.round((target[i] - offset) / stride))))
+            idx = np.argmin(np.abs(np.log(target[3] / anchors)))
+            pos.append(idx)
+            flag = False
+        else:
+            idx = random.sample(range(len(iz)), 1)[0]
+            pos = [iz[idx], ih[idx], iw[idx], ia[idx]]
+        dz = (target[0] - oz[pos[0]]) / anchors[pos[3]]
+        dh = (target[1] - oh[pos[1]]) / anchors[pos[3]]
+        dw = (target[2] - ow[pos[2]]) / anchors[pos[3]]
+        dd = np.log(target[3] / anchors[pos[3]])
+        label[pos[0], pos[1], pos[2], pos[3], :] = [1, dz, dh, dw, dd]
+        return label        
 
 if __name__ == '__main__':
 
-    data_dir = sys.argv[1]
-    main(data_dir)
+    data_dir = '/Users/john/Projects/SOTAEvaluationNoduleDetection/models/grt123/prep_result'
+
+    config = {}
+    config['anchors'] = [ 10.0, 30.0, 60.]
+    config['chanel'] = 1
+    config['crop_size'] = [128, 128, 128]
+    config['stride'] = 4
+    config['max_stride'] = 16
+    config['num_neg'] = 800
+    config['th_neg'] = 0.02
+    config['th_pos_train'] = 0.5
+    config['th_pos_val'] = 1
+    config['num_hard'] = 2
+    config['bound_size'] = 12
+    config['reso'] = 1
+    config['sizelim'] = 1. #mm
+    config['sizelim2'] = 30
+    config['sizelim3'] = 40
+    config['aug_scale'] = True
+    config['r_rand_crop'] = 0.3
+    config['pad_value'] = 170
+    config['augtype'] = {'flip':True,'swap':False,'scale':True,'rotate':False}
+    config['blacklist'] = ['868b024d9fa388b7ddab12ec1c06af38','990fbe3f0a1b53878669967b9afd1441','adc3bbc63d40f8761c59be10f1e504c3']
+
+    dataset = DataBowl3Detector(
+                data_dir,
+                config,
+                phase = 'train'
+            )
