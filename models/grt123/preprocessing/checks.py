ccbc1afe7ca51ab89db2ab750e283ab623d2263c
john.mccabe44 <j.mccabe@ucl.com>
2024-04-30 14:47:44 +0100

    Added

---
+++ models/grt123/preprocessing/checks.py
@@ -0,0 +1,31 @@
+import logging
+from multiprocessing import Pool
+from pathlib import Path
+import numpy as np
+import sys
+from tqdm import tqdm
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='preprocessing.log',
+    filemode='w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+def get_size(path):
+    return np.load(path).shape
+
+def main(prepresult_path):
+
+    list_clean_files = list(Path(prepresult_path).rglob('clean.npy'))
+
+    with Pool(8) as p:
+        sizes = list(tqdm(p.imap(get_size, list_clean_files), total=len(list_clean_files)))
+
+
+    for filename, size in zip(list_clean_files, sizes):
+        logging.info(f'{filename.stem}: {size}')
+
+if __name__ == '__main__':
+    prepresult_path = sys.argv[1]
+    main(prepresult_path)
