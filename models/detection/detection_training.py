e0b283aab45db4708d29751e6fdddb4706d45438
john.mccabe44 <j.mccabe@ucl.ac.uk>
2024-11-13 11:01:35 +0000

    Remving debug statements

--- models/detection/detection_training.py
+++ models/detection/detection_training.py
@@ -316,7 +316,7 @@ def main():
     tensorboard_writer = SummaryWriter(args.tfevent_path)
 
     # 5. train
-    val_interval = 1  # do validation every val_interval epochs
+    val_interval = 10  # do validation every val_interval epochs
     coco_metric = COCOMetric(classes=["nodule"], iou_list=[0.1], max_detection=[100])
     best_val_epoch_metric = 0.0
     best_val_epoch = -1  # the epoch that gives best validation metrics
@@ -372,9 +372,6 @@ def main():
             epoch_box_reg_loss += outputs[detector.box_reg_key].detach().item()
             logging.debug(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
             tensorboard_writer.add_scalar("train_loss", loss.detach().item(), epoch_len * epoch + step)
-
-            if step == 10:
-                break
 
         end_time = time.time()
         logging.info(f"Training time: {end_time-start_time}s")
